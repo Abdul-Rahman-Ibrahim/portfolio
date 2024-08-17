9603db2e2e392d42cf661ed90e37371279984481
Abdul-Rahman Ibrahim <abdulrahmanibrahim.ish@gmail.com>
2024-08-17 21:10:54 +0300

    404 issue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ReactGA from 'react-ga';
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./pages/Home/Home";
@@ -19,18 +19,18 @@ if (typeof process.env.REACT_APP_TRACKING_ID !== 'undefined') {
 
 function App() {
     return (
-        <Router>
+        <Router basename="portfolio">
             <div className="App">
-                <NavBar/>
+                <NavBar />
                 <br />
-                <ScrollToTop/>
+                <ScrollToTop />
                 <Routes>
-                    <Route path={"/portfolio"} exact element={<Home/>}/>
-                    <Route path={"/projects"} exact element={<Projects/>}/>
-                    <Route path={"/blogs"} exact element={<Blogs/>}/>
-                    <Route path={"/skills"} exact element={<Skills/>}/>
+                    <Route path={"/portfolio"} exact element={<Home />} />
+                    <Route path={"/projects"} exact element={<Projects />} />
+                    <Route path={"/blogs"} exact element={<Blogs />} />
+                    <Route path={"/skills"} exact element={<Skills />} />
                 </Routes>
-                <Footer/>
+                <Footer />
             </div>
         </Router>
 
